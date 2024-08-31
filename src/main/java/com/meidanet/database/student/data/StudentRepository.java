d6365344473083d9d63c937e13197d339532dfcc
nikolni <128720902+nikolni@users.noreply.github.com>
2024-08-31 17:09:11 +0300

    Update StudentRepository.java

--- src/main/java/com/meidanet/database/student/data/StudentRepository.java
+++ src/main/java/com/meidanet/database/student/data/StudentRepository.java
@@ -1,9 +1,14 @@
 package com.meidanet.database.student.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long >
 {
+    @Query("SELECT c FROM Student c WHERE c.student_id = :student_id")
+    List<Student> findByStudent_id(String student_id);
 }
